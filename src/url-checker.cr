1baa17878c7e274ca1e7ea4b8f12fc6e8f88a61a
Erik Griffin <griffin.erik@gmail.com>
2020-05-31 23:32:35 -0600

    Basic calling of urls from yml file

--- src/url-checker.cr
+++ src/url-checker.cr
@@ -1,6 +1,29 @@
-# TODO: Write documentation for `Url::Checker`
-module Url::Checker
-  VERSION = "0.1.0"
+require "http/client"
+require "yaml"
 
-  # TODO: Put your code here
-end
+# Alternate syntax: Proc(Arg1Type, ReturnType).new {}
+get_urls = -> {
+  # It's also possible to define a schema for the
+  #   yaml file you are reading.
+  urls_file = File.read "./urls.yml"
+  YAML.parse(urls_file)["urls"].as_a.map(&.as_s)
+}
+# ? Why use a Proc instead of a method definition ?
+#   Interesting.. a proc can be passed to a map
+#   as the block argument.
+
+get_status = Proc(String, {String, Int32 | Exception}).new {|url|
+  begin
+    puts "Calling #{url}"
+    res = HTTP::Client.get url
+    {url, res.status_code}
+  rescue e : Socket::ConnectError | Socket::Addrinfo::Error
+    {url, e}
+  ensure
+    puts "Done!"
+  end
+}
+
+puts get_urls.call.map(&get_status).join("\n")
+
+puts "Goodbye, cruel world."
