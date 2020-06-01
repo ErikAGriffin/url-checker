92fd4219589bab24ae4a7412d394e77e4dbca9f8
Erik Griffin <griffin.erik@gmail.com>
2020-05-31 23:56:15 -0600

    Checking urls concurrently

--- src/url-checker.cr
+++ src/url-checker.cr
@@ -2,15 +2,14 @@ require "http/client"
 require "yaml"
 
 # Alternate syntax: Proc(Arg1Type, ReturnType).new {}
-get_urls = -> {
+def get_urls
   # It's also possible to define a schema for the
   #   yaml file you are reading.
   urls_file = File.read "./urls.yml"
   YAML.parse(urls_file)["urls"].as_a.map(&.as_s)
-}
-# ? Why use a Proc instead of a method definition ?
-#   Interesting.. a proc can be passed to a map
-#   as the block argument.
+end
+
+puts "Starting Program"
 
 get_status = Proc(String, {String, Int32 | Exception}).new {|url|
   begin
@@ -24,6 +23,41 @@ get_status = Proc(String, {String, Int32 | Exception}).new {|url|
   end
 }
 
-puts get_urls.call.map(&get_status).join("\n")
+url_stream = Channel(String).new
+result_stream = Channel({String, Int32 | Exception}).new
+
+puts "Spawning get_urls fiber"
+# Tradeoffs of one fiber here vs. a fiber for each
+# url? .send will block this fiber until .receive is called
+# What is the cost of spinning up tons of fibers?
+spawn do
+  puts "Iterating over urls"
+  get_urls.each do |url|
+    puts "got a url: #{url}"
+    # Channels are blocking when send is called on them
+    url_stream.send url
+  end
+  # get_urls.each { |url| url_stream.send url }
+end
+
+2.times do
+  puts "Spawning Worker"
+  spawn do
+    loop do
+      puts "Receiving url"
+      url = url_stream.receive
+      result = get_status.call url
+      puts "Sending Result #{url}: #{result.last}"
+      result_stream.send result
+    end
+  end
+end
+
+puts "looping result_stream.receive.."
+loop do
+  puts result_stream.receive
+end
+
+# puts get_urls.map(&get_status).join("\n")
 
 puts "Goodbye, cruel world."
