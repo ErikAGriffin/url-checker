b16dd7cfad51a04cb2f9c092a8ebe0b19e2b89a3
Erik Griffin <griffin.erik@gmail.com>
2020-06-03 18:20:47 -0600

    Set up Logging macro module

---
+++ src/lib/logging.cr
@@ -0,0 +1,34 @@
+require "log"
+
+backend = Log::IOBackend.new
+backend.formatter = ->(entry : Log::Entry, io : IO) {
+  label = entry.severity.label
+  source = entry.source
+  io << label <<
+  " [" << entry.timestamp.to_s("%H:%M:%S") << "]" <<
+  "#" << source << "." << Fiber.current.name <<
+  ": " << entry.message
+  if entry.context.size > 0
+    io << " -- " << entry.context
+  end
+  if ex = entry.exception
+    io << " -- " << ex.class << ": " << ex
+  end
+}
+Log.builder.bind "*", :debug, backend
+
+# Log.builder.bind "db.*", :warning, backend
+
+# Allows for auto-instantiation of a
+# Log constant within the given class.
+module Logging
+  macro extended
+    # How might I use the `self` of the
+    # extended module here?
+    # Also, I can use macro if statements
+    # to change this initialization if the
+    # @type == Program, to not wrap my main
+    # file in a module, if that's something I want
+    Log = ::Log.for {{@type.stringify}}
+  end
+end
--- src/url-checker.cr
+++ src/url-checker.cr
@@ -1,30 +1,41 @@
 require "./lib/config"
+require "./lib/logging"
 require "./lib/stats"
 require "./lib/concurrency_util"
 require "./lib/tasks/status_checker"
 require "./lib/tasks/stats_logger"
 require "./lib/tasks/printer"
 
-puts "Starting Program"
+# extend Logging
+module Main
+  extend self
+  extend Logging
 
-CONFIG  = Config.load
-WORKERS = CONFIG.workers
-PERIOD  = CONFIG.period
+  CONFIG  = Config.load
+  WORKERS = CONFIG.workers
+  PERIOD  = CONFIG.period
 
-url_stream = Channel(String).new
-url_status_stream = Channel({String, Int32 | Exception}).new
-stats_stream = Channel(Stats::StatStream).new
+  def run
+    Log.info { "Starting Program" }
 
-every PERIOD do
-  Config.load.urls >> url_stream
+    url_stream = Channel(String).new
+    url_status_stream = Channel({String, Int32 | Exception}).new
+    stats_stream = Channel(Stats::StatStream).new
+
+    every PERIOD do
+      Config.load.urls >> url_stream
+    end
+
+    WORKERS.times do
+      StatusChecker.run(url_stream, url_status_stream)
+    end
+
+    StatsLogger.run(url_status_stream, stats_stream)
+
+    Printer.run(stats_stream)
+
+    sleep
+  end
 end
 
-WORKERS.times do
-  StatusChecker.run(url_stream, url_status_stream)
-end
-
-StatsLogger.run(url_status_stream, stats_stream)
-
-Printer.run(stats_stream)
-
-sleep
+Main.run
